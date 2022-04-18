2df4dbbf7b042e5bc59d14b37d03ed3080f6bd27
Landon Schropp <schroppl@gmail.com>
2022-04-17 22:02:29 -0500

    Add some temporary filler content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,22 @@ import { Layout } from "../components/layout";
 
 export default function IndexPage() {
   return <Layout>
-    <h1 className="m-5 text-3xl font-bold underline">
+    <h1>
       Hello world!
     </h1>
-    <p>Something else</p>
+    <p>Something else <a href="#">Hello</a></p>
+    <blockquote>Hello there friend. <cite>John Denver</cite></blockquote>
+    <hr />
+    <img src="https://www.placecage.com/1000/800" />
+    <ol>
+      <li>One</li>
+      <li>two</li>
+      <li>three</li>
+    </ol>
+    <ul>
+      <li>One</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
   </Layout>;
 }
