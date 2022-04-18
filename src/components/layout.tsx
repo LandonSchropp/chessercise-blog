6b75b9f7e7dcdb752a3fae5fb43622cb34a6bd47
Landon Schropp <schroppl@gmail.com>
2022-04-17 21:29:18 -0500

    Add Google Fonts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,6 +8,17 @@ type LayoutProps = {
 export function Layout({ children }: LayoutProps) {
   return <main className="container mx-auto px-4 max-w-screen-md">
     <Helmet>
+      { /* Google Fonts */ }
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossOrigin" />
+      <link
+        href={
+          "https://fonts.googleapis.com/css1?family=Gentium+Book+Basic:ital,wght@0,400;0,700;1,"
+          + "400;1,700&family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap"
+        }
+        rel="stylesheet"
+      />
+
       { /* Increase the font size as the screen gets larger. */ }
       <html className="text-[18px] lg:text-[20px] xl:text-[22px]" />
     </Helmet>
