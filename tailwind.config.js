7fcc8a898f8cb47781d8814638af23819ec08ad1
Landon Schropp <schroppl@gmail.com>
2022-04-17 21:29:53 -0500

    Configure the Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
+    colors: ({ colors }) => {
+      return {
+        inherit: colors.inherit,
+        current: colors.current,
+        transparent: colors.transparent,
+
+        mediumPurple: "#836fdd",
+
+        blackOut: "#1b1c21",
+        mineShaft: "#292b33",
+        emperor: "#484a54",
+        gray: "#81838f",
+        greatFalls: "#a1a4b3",
+        cerebral: "#bec1cc",
+        steam: "#d3d5de",
+        bleach: "#ebecf2",
+        white: "#fff"
+      };
+    },
+    fontFamily: {
+      sans: [ "Open Sans", "sans-serif" ],
+      serif: [ "Gentium Book Basic", "serif" ],
+      mono: [ "Source Code Pro", "monospace" ]
+    }
   },
   plugins: []
 };
