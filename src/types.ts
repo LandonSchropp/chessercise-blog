da0c57e9b20d0c5ac00b7ca32de3b585239a97c6
Landon Schropp <schroppl@gmail.com>
2022-07-11 12:43:55 -0500

    Add additional types

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,24 @@ export type Article = {
   description: string,
   published: boolean
 };
+
+export type Coordinates = [
+  number,
+  number
+];
+
+export type Player = "white" | "black";
+
+export type PlayerPiece =
+  | "blackKing"
+  | "blackQueen"
+  | "blackRook"
+  | "blackBishop"
+  | "blackKnight"
+  | "blackPawn"
+  | "whiteKing"
+  | "whiteQueen"
+  | "whiteRook"
+  | "whiteBishop"
+  | "whiteKnight"
+  | "whitePawn";
